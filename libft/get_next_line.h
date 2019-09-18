6a0c3862cfc382a278822e838b7822c940120ad4
brokenFdreams <brokenFdreams@protonmail.com>
2019-09-18 16:56:24 +0300

    ft_error

---
+++ libft/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsinged <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/17 11:16:02 by fsinged           #+#    #+#             */
+/*   Updated: 2019/07/19 14:01:11 by fsinged          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 42
+
+int					get_next_line(const int fd, char **line);
+
+#endif
