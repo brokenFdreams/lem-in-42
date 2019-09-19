86b98bb5c3a5adcf789fe683ef85452255e131ed
brokenFdreams <brokenFdreams@protonmail.com>
2019-09-19 16:25:40 +0300

    validation

--- src/validation.c
+++ src/validation.c
@@ -6,7 +6,7 @@
 /*   By: fsinged <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 14:44:07 by fsinged           #+#    #+#             */
-/*   Updated: 2019/09/19 15:04:22 by fsinged          ###   ########.fr       */
+/*   Updated: 2019/09/19 16:24:12 by fsinged          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,87 @@
 ** Check fisrt str in file
 */
 
-static int	read_ants(char *str)
+static int	read_num(char *str)
 {
-	int ants;
+	int num;
 
-	ants = ft_atoi(str);
+	num = ft_atoi(str);
 	while (*str)
 	{
 		if (!ft_isdigit(*str))
 			ft_error("Error");
 		str++;
 	}
-	return (ants);
+	return (num);
 }
 
-int			validation(char **data)
+/*
+** Check str, room is it or not
+*/
+
+static int	isroom(char *str)
 {
-	int error;
-	int	ants;
+	if (*str == 'L' || *str == '#')
+		return (0);
+	while (*str && *str != ' ')
+		str++;
+	str++;
+	while (*str && *str != ' ')
+		if (!ft_isdigit(*str))
+			return (0);
+		else
+			str++;
+	str++;
+	while (*str && *str != '\n')
+		if (!ft_isdigit(*str))
+			return (0);
+		else
+			str++;
+	return (1);
+}
+
+/*
+** Count the number of rooms
+*/
+
+static int	count_rooms(char **data)
+{
+	int count;
 	int i;
 
+	count = 0;
+	i = -1;
+	while (data[++i])
+		if (isroom(data[i]))
+			count++;
+	return (count);
+}
+
+int			validation(char **data, char ***rooms, char ***ways)
+{
+	int	ants;
+	int i;
+	int rcount;
+	int wcount;
+
 	i = 0;
+	rcount = 0;
+	wcount = 0;
 	while (data[i][0] == '#' && data[i][1] != '#')
 		i++;
-	ants = read_ants(data[i]);
-	while (data[i])
-	{
-		if (ft_strcmp(data[i],  "##start") == 0 ||
+	ants = read_num(data[i]);
+	while (++i && data[i])
+		if (!(ft_strcmp(data[i],  "##start") == 0 ||
 			ft_strcmp(data[i], "##end") == 0 ||
-			(data[i][0] == '#' && data[i][1] != '#') ||
-			
-		i++;
-	}
-
+			(data[i][0] == '#' && data[i][1] != '#')))
+		{
+			if (isroom(data[i]))
+				rcount++;
+			else if (isway)
+				wcount++;
+			else
+				break ;
+		}
+	fill_ways(data + fill_rooms(data, rooms, rcount), ways, wcount);
 	return (ants);
 }
